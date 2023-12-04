fa5e404e1d6222a43c0383f48253aa21580aea15
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2023-12-04 13:08:00 +0100

    added feeder functionality and gpiozero packages

--- AutomaticFeeder.py
+++ AutomaticFeeder.py
@@ -2,15 +2,24 @@ import torch
 import os
 import time
 import cv2
+from gpiozero import MotionSensor
+from gpiozero import Servo
 
 class AutomaticFeeder:
     def __init__(self, camera, delay, num_images) -> None:
         self.detections = []
-        self.image_path = 'C:/Users/Florentin/Documents/Schule/Spl/FinalDogDetection/test_images/'
+        self.image_path = 'test_images/'
+        self.images = []
         self.device = cv2.VideoCapture(camera)
         self.delay = delay
+        self.first_delay = delay * 1.5
         self.num_images = num_images
         self.model = 0
+        self.pir = MotionSensor(18)
+        self.motor = Servo(17)
+
+    def motion_detection(self):
+        self.pir.when_activated = self.take_pictures()
 
     def take_pictures(self):
         if not os.path.exists(self.image_path):
@@ -19,68 +28,62 @@ class AutomaticFeeder:
         if not self.device.isOpened():
             print("Error: Could not open camera.")
             return
+        
+        time.sleep(self.first_delay)
 
         for i in range(self.num_images):
             time.sleep(self.delay)
             try:
-                ret, picture = self.device.read()
+                ret, frame = self.device.read()
 
             except Exception as e:
                 print(e)
 
             try:
                 filename = os.path.join(self.image_path, f'image_{i+1}.jpg')
-                print(filename)
-                cv2.imwrite(filename, picture)
+                self.images.append(filename)
+                cv2.imwrite(filename, frame)
                 
             except Exception as e:
                 print(e)
 
-            finally:
-                self.device.release()
-                cv2.destroyAllWindows()
+        self.device.release()
+        cv2.destroyAllWindows()
 
     def detect(self):
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-        detections = self.model(self.image_path)
-        detections.show()
+        #self.model.classes = [15,16]
 
+        for image in self.images:   
+            results = self.model(image)
+            #results.show()
 
-    def clean_image_folder():
-        pass
+            prediction = results.pred[0]
+            class_names = results.names
+            
+            for det in prediction:
+                label_index = int(det[-1])
+                class_name = class_names[label_index]
+                self.detections.append(class_name)
 
     def get_detections(self):
+        print(self.detections)
         return self.detections
 
+    def open_lid(self):
+        self.motor
 
-feeder = AutomaticFeeder(camera = 1, delay = 0.5, num_images = 5)
+
+feeder = AutomaticFeeder(camera = 0, delay = 0.5, num_images = 5)
+
+
+feeder.motion_detection()
+
+print('taking_pictures')
 feeder.take_pictures()
+
+print('detecting')
 feeder.detect()
 
-def detect_animal(self, image_path):
-        results = model(image_path)
-        results.show()
-        prediction = results.pred[0]
-        class_names = results.names
-
-        classes = []
-        for det in prediction:
-            label_index = int(det[-1])
-            class_name = class_names[label_index]
-            classes.append(class_name)
-
-        return classes
-
-    
-def classify_animal(self, classes):
-        if 'dog' in classes:
-            return 'Hund'
-
-        sorted_images = os.listdir(self.output_folder)
-        for image in sorted_images:
-
-            image_path = os.path.join(self.output_folder,image)
-            classes = self.detect_animal(image_path)
-            animal_type = self.classify_animal(classes)
-            print(animal_type)
-
+print('getting detections')
+feeder.get_detections()
