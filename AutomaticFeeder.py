1b87df1719b4591d2e25e744cf740a720ec9fbb5
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2024-01-15 12:49:09 +0100

    added AutomaticFeeder and motor

--- AutomaticFeeder.py
+++ AutomaticFeeder.py
@@ -1,9 +1,10 @@
 import torch
 import os
-import time
 import cv2
+from time import sleep
 from gpiozero import MotionSensor
 from gpiozero import Servo
+from gpiozero.pins.mock import MockFactory
 
 class AutomaticFeeder:
     def __init__(self, camera, delay, num_images) -> None:
@@ -15,11 +16,34 @@ class AutomaticFeeder:
         self.first_delay = delay * 1.5
         self.num_images = num_images
         self.model = 0
-        self.pir = MotionSensor(18)
-        self.motor = Servo(17)
+    	
+        myCorrection=0.45
+        maxPW=(2.0+myCorrection)/1000
+        minPW=(1.0-myCorrection)/1000
+
+        self.servo = Servo(27)
+        print("Calibrating the servo...")
+        self.servo.min()
+        sleep(1)
+        self.servo.mid()
+        sleep(1)
+        self.servo.max()
+        sleep(1)
+        print("Calibration complete.")
+        self.servo.detach()
+
+        self.motion_sensor = MotionSensor(17)
 
     def motion_detection(self):
-        self.pir.when_activated = self.take_pictures()
+        motion = 0
+        while motion == 0:
+            print('Detecting Movement')
+            self.motion_sensor.wait_for_active()
+            motion = 1
+            sleep(1)
+
+        print('taking_pictures')
+        self.take_pictures()
 
     def take_pictures(self):
         if not os.path.exists(self.image_path):
@@ -28,11 +52,42 @@ class AutomaticFeeder:
         if not self.device.isOpened():
             print("Error: Could not open camera.")
             return
+
+        try:
+            sleep(self.first_delay)
+
+            for i in range(self.num_images):
+                sleep(self.delay)
+                ret, frame = self.device.read()
+
+                filename = os.path.join(self.image_path, f'image_{i+1}.jpg')
+                self.images.append(filename)
+                cv2.imwrite(filename, frame)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            # Release the camera and destroy OpenCV windows regardless of exceptions
+            self.device.release()
+            cv2.destroyAllWindows()
+
+        print('Detecting')
+        self.detect()
+
+
+    def old_take_pictures(self):
+        if not os.path.exists(self.image_path):
+            os.makedirs(self.image_path)
+
+        if not self.device.isOpened():
+            print("Error: Could not open camera.")
+            return
         
-        time.sleep(self.first_delay)
+        sleep(self.first_delay)
 
         for i in range(self.num_images):
-            time.sleep(self.delay)
+            sleep(self.delay)
             try:
                 ret, frame = self.device.read()
 
@@ -49,6 +104,9 @@ class AutomaticFeeder:
 
         self.device.release()
         cv2.destroyAllWindows()
+        
+        print('Detecting')
+        self.detect()
 
     def detect(self):
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
@@ -66,24 +124,25 @@ class AutomaticFeeder:
                 class_name = class_names[label_index]
                 self.detections.append(class_name)
 
-    def get_detections(self):
-        print(self.detections)
-        return self.detections
+        #if ('dog' in self.detections):
+        print('Opening Lid!')
+        self.open_lid()
+        #else:
+        #    pass
 
     def open_lid(self):
-        self.motor
-
+        self.servo.value = 0.5  # Set to mid position
+        sleep(0.5)
+        self.servo.value = 0.0  # Set to min position
+        sleep(1)
+        self.servo.value = 0.5  # Set back to mid position
+        sleep(0.5)
+        self.servo.value = 1.0  # Set to max position
+        sleep(1)
+        self.servo.detach()
 
 feeder = AutomaticFeeder(camera = 0, delay = 0.5, num_images = 5)
 
-
+print('Starting')
 feeder.motion_detection()
 
-print('taking_pictures')
-feeder.take_pictures()
-
-print('detecting')
-feeder.detect()
-
-print('getting detections')
-feeder.get_detections()
--- Externals/motor.py
+++ Externals/motor.py
@@ -1,31 +1,22 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Servo
+from gpiozero import MotionSensor
+from time import sleep
+from gpiozero.pins.mock import MockFactory
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+servo = Servo(17)
+servo.pin_factory = MockFactory()
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
-p.start(2.5) # Initialisierung
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-    
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+servo.min()
+
+motion = MotionSensor(27)
+
+
+motion.wait_for_active()
+sleep(1)
+
+servo.max()
+sleep(1)
+servo.min()
+sleep(1)
+
+servo.detach()
